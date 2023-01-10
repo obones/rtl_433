f662fae5ad1dce3ee504e91d0a37c5e842c3cd87
OBones <obones@free.fr>
2023-01-10 16:03:20 +0100

    feat: Introduce RTL_433_DISABLE_DECODER_LOG to allow completely removing log content from the final binary (useful on low memory systems)

--- include/decoder_util.h
+++ include/decoder_util.h
@@ -40,6 +40,7 @@ void decoder_output_data(r_device *decoder, data_t *data);
 /// Output log.
 void decoder_output_log(r_device *decoder, int level, data_t *data);
 
+#ifndef RTL_433_DISABLE_DECODER_LOG
 // be terse, a maximum msg length of 60 characters is supported on the decoder_log_ functions
 // e.g. "FoobarCorp-XY3000: unexpected type code %02x"
 
@@ -72,5 +73,19 @@ void decoder_logf_bitrow(r_device *decoder, int level, char const *func, uint8_t
         __attribute__((format(printf, 6, 7)))
 #endif
         ;
+#else
+// use "empty macros" so that the unused arguments given hard coded strings will not appear in the final binary
+// Note that te decoder_logf method uses its variadic arguments to avoid emitting warnings about unused variables in some decoders
+// calling printf with an empty format string to get a noop. But as this emits a warning, we disable it.
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wformat-zero-length"
+#endif
+#define decoder_log(decoder, level, func, msg) /**/
+#define decoder_logf(decoder, level, func, format, ...) printf("", ## __VA_ARGS__) /**/
+#define decoder_log_bitbuffer(decoder, level, func, bitbuffer, msg) /**/
+#define decoder_logf_bitbuffer(decoder, level, func, bitbuffer, format, ...) /**/
+#define decoder_log_bitrow(decoder, level, func, bitrow, bit_len, msg) /**/
+#define decoder_logf_bitrow(decoder, level, func, bitrow, bit_len, format, ...) /**/
+#endif
 
 #endif /* INCLUDE_DECODER_UTIL_H_ */
